b4cc6050d6a70d333f943a9a56ffa1959fe67b3e
Roma Patel <patel.roma21@gmail.com>
2020-02-05 21:32:54 -0500

    test files for flask app

---
+++ trial_app/views.py
@@ -0,0 +1,52 @@
+from flask import request, redirect, Flask
+from werkzeug.utils import secure_filename
+import os
+
+# app.config["IMAGE_UPLOADS"] = "/Users/romapatel/Desktop/trial_app/IMAGE_UPLOADS"
+# app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG", "GIF"]
+
+app = Flask(__name__)
+
+
+def allowed_image(filename):
+
+    if not "." in filename:
+        return False
+
+    ext = filename.rsplit(".", 1)[1]
+
+    if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+        return True
+    else:
+        return False
+
+@app.route("/upload-image", methods=["GET", "POST"])
+def upload_image():
+
+    if request.method == "POST":
+
+        if request.files:
+
+            image = request.files["image"]
+
+            if image.filename == "":
+                print("No filename")
+                return redirect(request.url)
+
+            if allowed_image(image.filename):
+                filename = secure_filename(image.filename)
+
+                image.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
+
+                print("Image saved")
+
+                return redirect(request.url)
+
+            else:
+                print("That file extension is not allowed")
+                return redirect(request.url)
+
+    return render_template("../upload_image.html")
+
+
+    
