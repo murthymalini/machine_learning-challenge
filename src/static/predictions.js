b1dd326a614483b48078b011835991725b2195af
Cesar Mosquera <cemoga@Cesars-MacBook-Air.local>
2020-02-07 14:09:54 -0500

    Predictions in console

--- src/static/predictions.js
+++ src/static/predictions.js
@@ -1,8 +1,23 @@
 var url = `/predict`;
 
+var schoolName = [],
+    model = [],
+    no = [],
+    prediction = [],
+    probability = [];
+
 d3.json(url).then((data) => {
-
-    console.log(data)
-
-
+    // console.log(data)
+    for (i = 0; i < 3; i++) {
+        data[i].forEach((item) => {
+            model.push(item.Model)
+            no.push(item.no)
+            prediction.push(item.prediction)
+            probability.push(item.probability)
+        })
+    }
+    console.log(model)
+    console.log(no)
+    console.log(prediction)
+    console.log(probability
 })
